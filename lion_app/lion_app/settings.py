06f7125916e0d55fa0ab87db4f89451cf634f9e2
Ivan Kim <xormrdlsrks2@gmail.com>
2023-08-07 15:09:57 +0900

    Update for applying db env

--- lion_app/lion_app/settings.py
+++ lion_app/lion_app/settings.py
@@ -93,9 +93,17 @@ WSGI_APPLICATION = 'lion_app.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
+
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('POSTGRES_DB', 'postgres'),
+        'USER': os.getenv('POSTGRES_USER', 'postgres'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD', 'postgres'),
+        'HOST': os.getenv('DB_HOST', 'db'),
     }
 }
 
