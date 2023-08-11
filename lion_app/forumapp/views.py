a351ef5f1aba342b6ea0f8b04eee262160118c4f
Ivan Kim <xormrdlsrks2@gmail.com>
2023-08-11 11:20:50 +0900

    Update code2: update views to check deployment

--- lion_app/forumapp/views.py
+++ lion_app/forumapp/views.py
@@ -5,7 +5,7 @@ from .models import Topic, Post
 from .serializers import TopicSerializer, PostSerializer
 
 # 모델 뷰셋 사용
-@extend_schema(tags=["Topic", "Forum"])
+@extend_schema(tags=["Topic"])
 class TopicViewSet(viewsets.ModelViewSet):
     # 어떤 모델 오브젝트를 쓸꺼니? 
     # all() -> create ~ list ~ 전부 알아서 작성됨
@@ -13,7 +13,11 @@ class TopicViewSet(viewsets.ModelViewSet):
     # 어떤 시리얼라이저 쓸꺼니?
     serializer_class = TopicSerializer
 
-@extend_schema(tags=["Post", "Forum"])
+    @extend_schema(summary="Create new topic")
+    def create(self, request, *args, **kwargs):
+        return super().create(request, *args, **kwargs)
+
+@extend_schema(tags=["Post"])
 class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
