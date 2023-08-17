aa686309e62211294de31f28cd797a87f53d5082
Ivan Kim <xormrdlsrks2@gmail.com>
2023-08-17 10:11:28 +0900

    FEAT forumapp views.py

--- lion_app/forumapp/views.py
+++ lion_app/forumapp/views.py
@@ -1,7 +1,12 @@
-from rest_framework import viewsets
+from django.shortcuts import get_object_or_404
+from django.db.models import Q
+from rest_framework import viewsets, status
 from drf_spectacular.utils import extend_schema
+from rest_framework.request import Request
+from rest_framework.response import Response
+from rest_framework.exceptions import PermissionDenied
 
-from .models import Topic, Post
+from .models import Topic, Post, TopicGroupUser
 from .serializers import TopicSerializer, PostSerializer
 
 
@@ -23,3 +28,27 @@ class TopicViewSet(viewsets.ModelViewSet):
 class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
+
+    def create(self, request: Request, *args, **kwargs):
+        # check group and topic if user has right permission to write a post
+        # return 403 forbidden
+        data = request.data
+        topic_id = data.get("topic")
+        topic = get_object_or_404(Topic, id=topic_id)
+
+        if topic.is_private:
+            qs = TopicGroupUser.objects.filter(
+                # Q(group=0) | Q(group=1),
+                group_lte=TopicGroupUser.groupChoices.common,
+                topic=topic,
+                user=user,
+            )
+
+            if not qs.exists():
+                return Response(
+                    status=status.HTTP_401_UNAUTHORIZED,
+                    data="This user is not allowed to write a post on this Topic",
+                )
+                # raise PermissionDenied("Forbidden")
+
+        return super().create(request, *args, **kwargs)
