c2c2b3e7c3e6221aee604f6cb1a4511e09022c71
Ivan Kim <xormrdlsrks2@gmail.com>
2023-08-21 15:14:26 +0900

    FEAT infra/aws/main.tf vpc, user creations

---
+++ infra/aws/main.tf
@@ -0,0 +1,65 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "ap-northeast-2"
+}
+
+# Create a VPC
+resource "aws_vpc" "lion" {
+  cidr_block = "10.1.0.0/16"
+
+  tags = {
+    Name = "lion-vpc"
+  }
+}
+
+
+# Create IAM user
+resource "aws_iam_user" "lion" {
+  name = "lion-tf"
+  path = "/"
+
+}
+
+resource "aws_iam_access_key" "lion" {
+  user = aws_iam_user.lion.name
+}
+
+data "aws_iam_policy_document" "lion_ro" {
+  statement {
+    effect    = "Allow"
+    actions   = ["ec2:Describe*"]
+    resources = ["*"]
+  }
+}
+
+resource "aws_iam_user_policy" "lion_ro" {
+  name   = "tf-test"
+  user   = aws_iam_user.lion.name
+  policy = data.aws_iam_policy_document.lion_ro.json
+}
+
+# active console access
+resource "aws_iam_user_login_profile" "lion" {
+  user    = aws_iam_user.lion.name
+#     lifecycle {
+#         ignore_changes = [
+#         password_length,
+#         password_reset_required,
+#         pgp_key,
+#         ]
+#   }
+}
+
+output "password" {
+  value = aws_iam_user_login_profile.lion.password
+  sensitive = true # console 에서는 가려주자
+}
