45c26fb5fd1d6925ee50b5de98614592e4367bff
Ivan Kim <xormrdlsrks2@gmail.com>
2023-08-24 17:21:32 +0900

    Update code: rectify sequence due to DJANGO key

--- scripts/deploy_prod_setup_file.sh
+++ scripts/deploy_prod_setup_file.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-scp set_be_server.sh lion@be-prod-server:/home/lion
+scp set_be_server.sh lion@be-server-prod:/home/lion
 
-scp set_db_server.sh lion@db-prod-server:/home/lion
+scp set_db_server.sh lion@db-server-prod:/home/lion
--- scripts/deploy_staging_setup_file.sh
+++ scripts/deploy_staging_setup_file.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-scp set_be_server.sh lion@be-staging:/home/lion
+scp set_be_server.sh lion@be-server-staging:/home/lion
 
-scp set_db_server.sh lion@db-staging:/home/lion
+scp set_db_server.sh lion@db-server-staging:/home/lion
