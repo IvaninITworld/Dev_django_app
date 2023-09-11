3af52e5a5d83695633e448f700273653c570e1bc
Ivan Kim <xormrdlsrks2@gmail.com>
2023-09-11 09:23:20 +0900

    UPDATE: add dns & rectify typo

--- lion_app/lion_app/settings/staging.py
+++ lion_app/lion_app/settings/staging.py
@@ -8,9 +8,11 @@ DEBUG = True
 
 ALLOWED_HOSTS = [
     "lion-lb-staging-18975818-470dadb487de.kr.lb.naverncp.com",  # Staging Load balancer
+    "default-lion-svc-lb-fab52-19456190-4c9ebc1a2d05.kr.lb.naverncp.com",
 ]
 
 CSRF_TRUSTED_ORIGINS = [
     "http://lion-lb-staging-18975818-470dadb487de.kr.lb.naverncp.com/",  # Staging Load balancer
     "http://223.130.145.30/",  # Staging Load balancer
+    "http://default-lion-svc-lb-fab52-19456190-4c9ebc1a2d05.kr.lb.naverncp.com/",  # Staging Load balancer"
 ]
