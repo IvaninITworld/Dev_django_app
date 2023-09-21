40ec6e2c25de0ce787523a5dd90e4e59f23aa32f
Ivan Kim <xormrdlsrks2@gmail.com>
2023-09-21 15:07:01 +0900

    UPDATE: quickstart app

--- lion_app/quickstart/serializers.py
+++ lion_app/quickstart/serializers.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import User, Group
 from rest_framework import serializers
+from .models import MenuItem, Category
 
 
 class UserSerializer(serializers.HyperlinkedModelSerializer):
@@ -12,3 +13,18 @@ class GroupSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Group
         fields = ["url", "name"]
+
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = ["id", "title"]
+
+
+class MenuItemSerializer(serializers.ModelSerializer):
+    category_id = serializers.IntegerField(write_only=True)
+    category = CategorySerializer(read_only=True)
+
+    class Meta:
+        model = MenuItem
+        fields = ["id", "title", "price", "inventory", "category", "category_id"]
--- lion_app/quickstart/views.py
+++ lion_app/quickstart/views.py
@@ -3,6 +3,9 @@ from django.contrib.auth.models import User, Group
 from rest_framework import viewsets
 from rest_framework import permissions
 from quickstart.serializers import UserSerializer, GroupSerializer
+from rest_framework import generics
+from .models import MenuItem, Category
+from .serializers import MenuItemSerializer, CategorySerializer
 
 
 # Create your views here.
@@ -24,3 +27,16 @@ class GroupViewSet(viewsets.ModelViewSet):
     queryset = Group.objects.all()
     serializer_class = GroupSerializer
     permission_classes = [permissions.IsAuthenticated]
+
+
+class CategoriesView(generics.ListCreateAPIView):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+
+
+class MenuItemsView(generics.ListCreateAPIView):
+    queryset = MenuItem.objects.all()
+    serializer_class = MenuItemSerializer
+    ordering_fields = ["price", "inventory"]
+    filterset_fields = ["price", "inventory"]
+    search_fields = ["title"]
